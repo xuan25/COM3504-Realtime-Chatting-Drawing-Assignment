5359deb78c0566eb1d4cb87c313b3b135d4185f1
aca18jq <jqi6@sheffield.ac.uk>
2021-03-17 00:53:35 +0800

    prepare for new index page
    Co-Authored-By: 瑄 <shanboxuan@me.com>
    Co-Authored-By: Lerry <45856796+ace18zz@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var deprecatedRouter = require('./routes/deprecated');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/deprecated', deprecatedRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- src/routes/deprecated.js
+++ src/routes/deprecated.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(404);
+  res.render('deprecated', { title: 'Image Browsing' });
 });
 
 module.exports = router;
