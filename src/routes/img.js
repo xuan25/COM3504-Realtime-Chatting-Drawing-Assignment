ae37ffbcd1dbf5aaa4aa41e2481029cb1eef4028
瑄 <shanboxuan@me.com>
2021-03-22 14:00:04 +0000

    Enhance img route compatibility

--- src/routes/img.js
+++ src/routes/img.js
@@ -5,8 +5,8 @@ const imgCon = require('../controllers/img');
 
 router.get('/all', imgCon.getAll);
 router.get('/meta', imgCon.getAllMeta);
-router.get(/^\/[0-9a-z]+$/, imgCon.getOne);
-router.get(/^\/meta\/[0-9a-z]+$/, imgCon.getOneMeta);
-router.get(/^\/raw\/[0-9a-z]+$/, imgCon.getOneRaw);
+router.get(/^\/[0-9a-z]+\/?$/, imgCon.getOne);
+router.get(/^\/meta\/[0-9a-z]+\/?$/, imgCon.getOneMeta);
+router.get(/^\/raw\/[0-9a-z]+\/?$/, imgCon.getOneRaw);
 
 module.exports = router;
--- src/routes/join.js
+++ src/routes/join.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const joinCon = require('../controllers/join');
 
 /* GET home page. */
-router.get(/^\/[0-9a-z]+$/, joinCon.getJoinPage);
+router.get(/^\/[0-9a-z]+\/?$/, joinCon.getJoinPage);
 
 module.exports = router;
