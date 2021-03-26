1ac8647b28553c6d0934a712169ac5c5a4282d32
瑄 <shanboxuan@me.com>
2021-03-26 18:04:27 +0000

    Inline comments for routes

--- src/routes/room.js
+++ src/routes/room.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 const roomCon = require('../controllers/room');
 
+// get room page for service worker
 router.get('/offline', roomCon.getRoomPageOffline);
+
+// get room page for a room
 router.get('/:imgId/:roomId', roomCon.getRoomPage);
 
 module.exports = router;
